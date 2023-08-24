32d5d124caff7233df079f0f04a9d7f4f7b5dc63
titus <titusaduket@gmail.com>
2023-08-24 09:45:51 +0300

    made task file readable

--- task0.c
+++ task0.c
@@ -1,5 +1,9 @@
 #include "monty.h"
 
+/**
+ * push - this function adds data into the stack
+ * @stack: this is the pointer to the node
+*/
 void push(stack_t **stack, unsigned int line_number)
 {
 	if (!job->args)
@@ -10,6 +14,10 @@ void push(stack_t **stack, unsigned int line_number)
 	add_node_end(stack, atoi(job->args));
 }
 
+/**
+ * pall - this function helps prints everything in the stack
+ * @stack: this is the pointer to the node
+*/
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = *stack;
