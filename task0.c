855ea7ad22a062c9c75b327cdd312043286cf671
titus <titusaduket@gmail.com>
2023-08-24 09:32:07 +0300

    fixing style

--- task0.c
+++ task0.c
@@ -2,28 +2,29 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
-    if (!job->args)
-    {
-        printf("L%d: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    add_node_end(stack, atoi(job->args));
+	if (!job->args)
+	{
+		printf("L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	add_node_end(stack, atoi(job->args));
 }
 
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *tmp = *stack;
-    if (!tmp)
-        return;
-    
-    (void) line_number;
+	stack_t *tmp = *stack;
 
-    while (tmp->next)
-        tmp = tmp->next;
-    
-    while (tmp)
-    {
-        printf("%d\n", tmp->n);
-        tmp = tmp->prev;
-    } 
+	if (!tmp)
+		return;
+
+	(void)line_number;
+
+	while (tmp->next)
+		tmp = tmp->next;
+
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->prev;
+	}
 }
